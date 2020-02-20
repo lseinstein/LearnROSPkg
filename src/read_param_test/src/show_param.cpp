25aa8801bdda272d83c00e45bda72c5bf95b2b2e
lseinstein <lseinstein@163.com>
2020-02-20 10:49:50 +0800

    initialized projekt

---
+++ src/read_param_test/src/show_param.cpp
@@ -0,0 +1,50 @@
+#include "ros/ros.h"
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    ros::init (argc, argv, "show_param");
+
+    ros::NodeHandle nh;
+
+    double noise;
+    if(nh.getParam("noise", noise))
+        ROS_INFO("noise is %f", noise);
+    else
+        ROS_WARN("didn't find parameter noise");
+
+    std::string string_var;
+    if (nh.getParam("string_var", string_var))
+        ROS_INFO("string_var: %s", string_var.c_str());
+    else
+        ROS_WARN("No string_var name message");
+
+    std::vector<int> a_list;
+    if (nh.getParam("a_list",a_list))
+        ROS_INFO("get a_list");
+    else
+        ROS_WARN("didn't find a_list");
+    
+    std::vector<int> vector_var;
+    if (nh.getParam("vector_var",vector_var))
+        ROS_INFO("got vector");
+    else
+        ROS_WARN("didn't find vector");
+
+/*
+ *    nh.getParam("noise", noise);
+ *    nh.getParam("string_var", string_var);
+ *    nh.getParam("a_list", a_list);
+ *
+ *    ROS_INFO("noise parameter is... %f", noise);
+ *    ROS_INFO("string_var parameter is... %s", string_var);
+ *    ROS_INFO("noise parameter is... %d", a_list[0]);
+ */
+
+    return 0;
+};
